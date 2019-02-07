4425c3873589baeaecf72d6aefe64b03d08be804
Michał Kowalik <michal.kowalik.94@gmail.com>
2019-02-07 23:07:59 +0100

    Updates

---
+++ precache-manifest.d83a0aa3f0b34cbda496c12fc9c9e66f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9206fede76c50103c02",
+    "url": "/ReactQuiz/static/js/runtime~main.c9206fed.js"
+  },
+  {
+    "revision": "41f7ce125c146d2cbb64",
+    "url": "/ReactQuiz/static/js/main.41f7ce12.chunk.js"
+  },
+  {
+    "revision": "ea989959bd7a2012d570",
+    "url": "/ReactQuiz/static/js/1.ea989959.chunk.js"
+  },
+  {
+    "revision": "41f7ce125c146d2cbb64",
+    "url": "/ReactQuiz/static/css/main.b28d247e.chunk.css"
+  },
+  {
+    "revision": "15cf8833cb27c42e05c8cf6c633de32a",
+    "url": "/ReactQuiz/index.html"
+  }
+];
